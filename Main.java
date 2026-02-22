9b7a398e9727cb372a33eb2b18e38b2b08f81396
omii93 <omii56211@gmail.com>
2026-02-22 23:15:47 +0530

    added  student class

--- Main.java
+++ Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-System.out.println("this is java program");
-System.out.println("output from java program");
-System.out.println(new Date());
-  System.out.println("hello");  
-  System.out.println("added one more file");    
+        System.out.println("this is java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+        System.out.println("hello");
+        System.out.println("added one more file");
+
+        Student stude = new Student();
+        stude.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public Student()
+    {
+        System.out.println("create student object");
+    }
+
+      public void sayHello()
+       {System.out.println("hi ,i am student ");
+        
+       }
+   
+}
